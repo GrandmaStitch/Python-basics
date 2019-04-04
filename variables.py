a323c3c1b8ea8aa641f7a1cbcc97e7811e4a7b5a
grandmasunshinessui <grandmasunshinessui@github.com>
2019-04-04 18:01:34 +0800

    Initial variables.py

---
+++ variables.py
@@ -0,0 +1,37 @@
+# A variable is a box for a value, which can be of various types
+
+# Data types
+# integer
+num1 = 1
+num2 = 2
+
+# string
+var1 = 'hello'
+var2 = 'world'
+any_variable_name = 'hello world'
+
+# float
+x = 1.0
+y = 1.55
+
+# boolean
+true_boolean = True
+false_boolean = False
+
+
+# Basic math
+total1 = num1 + num2
+total2 = num1 - num2
+total3 = num1 * num2
+total4 = num1 / num2
+total5 = num1 % num2
+
+
+# Casting
+a = str(num1)
+b = int(x)
+c = float(num1)
+print(f'{a}: {type(a)}')
+print(f'{b}: {type(b)}')
+print(f'{c}: {type(c)}')
+
