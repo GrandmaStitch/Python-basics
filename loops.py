2b2ae1429c097866aca733915e6082447b703f32
sui <grandmasunshinessui@github.com>
2019-04-06 21:53:41 +0800

    initial loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+my_string = 'hello python' # iterable
+# iterables are strings, lists, sets, tuples, and more
+
+# for loop
+for character in my_string:
+	print(character)
+
+
+# while loop
+i = 0
+while i < 10:
+	i += 1
+	print(i)
