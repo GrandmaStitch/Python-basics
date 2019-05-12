6ec4fe648cd481869d80c0ab65ccf0437a30d4d6
sui <grandmasunshinessui@github.com>
2019-05-12 20:08:39 +0800

    initial commit

--- Coding_Exercises/use_urllib.py
+++ Coding_Exercises/use_urllib.py
@@ -6,6 +6,11 @@ connection = urllib.request.urlopen('https://stackoverflow.com/')
 
 output = connection.read()
 
+print(type(output))
+
+output = str(output, 'iso-8859-1')
+output = output.rstrip('\r\n')
+
 print(output)
 
 connection.close()
@@ -14,16 +19,16 @@ connection.close()
 # urlparse
 o = urlparse('https://docs.python.org/3/library/urllib.parse.html')
 
-print(o)
-print(o.scheme)
-print(o.port)
-print(o.geturl())
+print('urlparse: {}'.format(o))
+print('urlparse.scheme: {}'.format(o.scheme))
+print('urlparse.port: {}'.format(o.port))
+print('urlparse.geturl: {}'.format(o.geturl()))
 
 
 # parse_qs
 q = parse_qs('q=abc&r=hi')
 
-print(q)
+print('parse_qs: {}'.format(q))
 
 
 # urlparse, parse_qs
@@ -46,17 +51,17 @@ print(query)
 # if that task isn’t already covered by the URL parsing functions above.
 quote1 = quote('a = Amy Money')
 
-print(quote1)
+print('quote: {}'.format(quote1))
 
 quote2 = quote_plus('a = Amy Money')
 
-print(quote2)
+print('quote plus: {}'.format(quote2))
 
 
 # unquote
 unquote = unquote(quote1)
 
-print(unquote)
+print('unquote: {}'.format(unquote))
 
 
 
